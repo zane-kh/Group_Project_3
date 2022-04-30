e13c4aabf2406384d70259d2b6a373d6eecb94e4
Zane <zkhimjisgu@gmail.com>
2022-04-30 10:43:40 -0700

    main_app created, database configured

--- Noah/settings.py
+++ Noah/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'main_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,9 @@ WSGI_APPLICATION = 'Noah.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'Noah'
     }
 }
 
