b4cd54ba5d40ff2c7f5ea7cebc70428327df055f
Zane <zkhimjisgu@gmail.com>
2022-04-30 10:55:50 -0700

    about page created

---
+++ main_app/urls.py
@@ -0,0 +1,8 @@
+from importlib.resources import path
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+   path('', views.home, name='home'),
+   path('about/', views.about, name='about')
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+
+def home (request):
+    return HttpResponse ('working so far')
+
+def about (request):
+    return HttpResponse('<h1> This is the About page </h1>')
