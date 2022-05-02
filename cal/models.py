e887ee60524c20408794066b795e64b8d3b6709f
Zane <zkhimjisgu@gmail.com>
2022-05-01 21:24:16 -0700

    appt service fk

--- cal/models.py
+++ cal/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.urls import reverse
-from main_app.models import Patient
+from main_app.models import Patient, Service
 
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    service = models.CharField(max_length=200, default='service')
     patient = models.ForeignKey(Patient, default=None, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, default=None, on_delete=models.CASCADE)
     description=models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
